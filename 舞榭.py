fc10a5e878a5e3ec45c9922445b217f83e1a32eb
如意The Magic Wand <196052567+Magic-Abracadabra@users.noreply.github.com>
2025-12-09 12:07:21 +0800

    Create 舞榭.py

---
+++ 舞榭.py
@@ -0,0 +1,28 @@
+from markdown import markdown
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QTextBrowser, QHBoxLayout
+from PyQt5.QtGui import QFont
+
+# 幕后
+幕后 = QApplication([])
+
+# 平台
+平台 = QWidget()
+平台.setWindowTitle('Markdown')
+
+# 舞者
+输入舞者 = QTextEdit()
+显示舞者 = QTextBrowser()
+
+# 舞者之间
+输入舞者.textChanged.connect(lambda: 显示舞者.setHtml(markdown(输入舞者.toPlainText())))
+
+# 布局
+布局 = QHBoxLayout()
+布局.addWidget(输入舞者)
+布局.addWidget(显示舞者)
+平台.setLayout(布局)
+
+# 幕后
+幕后.setFont(QFont('楷体', 32))
+平台.show()
+幕后.exec()
